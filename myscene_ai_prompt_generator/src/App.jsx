e3799ad2bda5da33e6c2efdb7fbd9627f2429c85
edgarcovarrubias <edgarignaciocovarrubiasc@gmail.com>
2025-07-13 18:31:30 -0600

    Add files via upload

---
+++ myscene_ai_prompt_generator/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import PromptGenerator from './PromptGenerator.jsx';
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-white text-black">
+      <PromptGenerator />
+    </div>
+  );
+}
